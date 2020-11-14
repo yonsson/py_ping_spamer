ff02b792f111c49ad9f53c773940f798f2aad6d5
yonsson <yonsson@gmail.com>
2020-11-14 20:09:38 +0300

    create response

--- ping_spamer.py
+++ ping_spamer.py
@@ -5,7 +5,7 @@ import requests
 import logging
 import time
 
-logging.basicConfig(format="%(levelname)s %(asctime)s %(message)s", level=logging.INFO)
+logging.basicConfig(format="%(levelname)s %(asctime)s %(message)s", level=logging.INFO, filename='logs.txt')
 
 load_dotenv()
 
@@ -14,6 +14,7 @@ TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
 FROM_PHONE_NUMBER = os.getenv('FROM_PHONE_NUMBER')
 TO_PHONE_NUMBER = os.getenv('TO_PHONE_NUMBER')
 URL = os.getenv('URL')
+SLEEP_TIME = 60
 
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
@@ -28,6 +29,17 @@ def send_sms(message_text):
     except Exception:
         logging.info(f'Сообщение не отправлено')
 
+count = 1
+while count <= 20:
+    response = requests.get(URL)
+    status = response.status_code
 
-response = requests.get(URL)
-print(response.status_code)
+    if status == 200:
+        logging.info(f'Сервер {URL} доступен')
+    else:
+        logging.info(f'{URL} выдал ошибку: {status} - отправляем смс')
+        send_sms(f'Сайт {URL} выдал ошибку: {status}')
+
+    time.sleep(SLEEP_TIME)
+    count += 1
+
